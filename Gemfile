5ea76c90328a92a5e44990aa0a21417aeda6f201
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-04-07 19:01:44 +0000

    adding links

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'bootstrap-sass', '~> 3.3.5.1'
+gem 'simple_form'
 gem 'sprockets', '2.11.0'
 
 gem 'rails_12factor', group: :production
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Splurty::Application.routes.draw do
 
   root 'quotes#index'
-
+  resources :quotes
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
