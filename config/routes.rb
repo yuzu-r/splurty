d5bff539ecd6db6e14e75aa0e7d2b6f56799e064
Tracy Kaneshiro <tkaneshiro@gmail.com>
2016-04-07 21:58:48 +0000

    adding content to about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Splurty::Application.routes.draw do
 
   root 'quotes#index'
   resources :quotes
-  
+  get 'about', to: 'quotes#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
